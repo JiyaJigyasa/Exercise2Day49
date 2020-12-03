bab1860c18a53800a3d82b2ac28aa7638179ee5e
JiyaJigyasa <Jiyajigyasa1@gmail.com>
2020-12-03 15:53:30 +0530

    first commit

---
+++ WebAppEmpEx2D49/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppEmpEx2D49.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
