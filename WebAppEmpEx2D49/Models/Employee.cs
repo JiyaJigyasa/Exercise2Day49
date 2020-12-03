3eb2b8a50d9e13d7690db323b39576736469643c
JiyaJigyasa <Jiyajigyasa1@gmail.com>
2020-12-03 15:58:15 +0530

    second commit

---
+++ WebAppEmpEx2D49/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppEmpEx2D49.Models
+{
+    public class Employee
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDoj { get; set; }
+    }
+}
